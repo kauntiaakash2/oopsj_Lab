91871605b9a2e4752e256175febae73d7e088f2f
kauntiaakash2 <akashkauntia2006@gmail.com>
2025-12-05 17:41:48 +0530

    Lab1_FifthProg

---
+++ ProgFifth.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class ProgFifth
+{
+    public static void main(String[] args)
+    {
+        String first, last;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Fist name: ");
+        first = sc.nextLine();
+        System.out.println("Enter Last name: ");
+        last = sc.nextLine();
+        System.out.println("Enter Full name: "+last+" "+first);
+    }
+}
